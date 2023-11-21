f0c75b7a3026dae148d7cc324cb6b1f91907e50d
WilliamFreedman <freedmanw@bxscience.edu>
2023-11-21 15:03:16 -0500

    changed main.py to not need the k value for svd. added a driver function to svd.py that iterates through all values of k (formerly done in main)

--- svd.py
+++ svd.py
@@ -34,5 +34,16 @@ def compress_svd(image_path, output_path, k):
     # Save the compressed image
     compressed_image.save(output_path)
 
+def svd_driver(image_path):
+    # Set the number of singular values to keep (compression factor)
+    original_size = os.path.getsize(image_path)
+    k_values = [2 ** i for i in range(20)]  # You can experiment with different values
 
-
+    # Apply SVD compression for each k value
+    for k in k_values:
+        output_path_k = f"compressed_image_k{k}.jpg"
+        compress_svd(image_path, output_path_k, k)
+        compression_ratio = os.path.getsize(output_path_k) / original_size
+        os.rename(output_path_k, "./output_images/svd/" + output_path_k)
+        print(
+            f"Compression with k={k} complete. Compression ratio: {compression_ratio}. Output saved to {output_path_k}")
